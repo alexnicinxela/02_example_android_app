affbcf99e920f2797c5e66c4d477097e01cddba7
Alexandr N <nogovitsin.ae@yandex.ru>
2022-05-22 00:18:11 +0300

    test size, padding, scroll and other

--- app/src/main/java/com/example/helloapp/MainActivity.kt
+++ app/src/main/java/com/example/helloapp/MainActivity.kt
@@ -5,6 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -12,6 +15,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.helloapp.ui.theme.HelloAppTheme
 
@@ -21,13 +25,29 @@ class MainActivity : ComponentActivity() {
         setContent {
             //val myColor: Color = Color(0xFFFF00)
 
-            val myColor: Color = Color(0xFF0000FF)
+            val myColor: Color = Color(0xFF34f755)
 
             Text(
-                "Alexander's App",
-                fontSize=25.sp,
-                modifier = Modifier.background(myColor)
-                    .fillMaxSize()
+                "Alexander's App\n" +
+                        "example\n" +
+                        "example\n" +
+                        "example\n" +
+                        "exmpl\n" +
+                        "What is Lorem Ipsum?\n" +
+                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.\n" +
+                        "\n" +
+                        "...............",
+                fontSize=30.sp,
+                modifier = Modifier
+                    //.padding(start=100.dp, top=250.dp)
+                    .background(myColor)
+                    .verticalScroll(rememberScrollState())
+                    //.size(width = 200.dp, height= 100.dp)
+                    //.padding(start=10.dp, top=30.dp)
+
+
+
+
             )
             /*HelloAppTheme {
                 // A surface container using the 'background' color from the theme
